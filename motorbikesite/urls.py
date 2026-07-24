34dc50cdbe2c219a7cb637845cb15d01a0342176
Moh2700 <mohhabbes@gmail.com>
2026-07-24 02:13:36 +0100

    Configure base homepage URL routing

--- motorbikesite/urls.py
+++ motorbikesite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('motorbike_rental.urls')),
     path('motorbike_rental/', include('motorbike_rental.urls')),
     path('admin/', admin.site.urls),
 ]
